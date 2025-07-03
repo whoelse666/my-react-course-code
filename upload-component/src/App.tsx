ee05e82d05f4fd3a026fce635907e9ad190c531d
whoelse <347300979@qq.com>
2025-07-03 21:16:25 +0800

    read ： 组件实战：Form表单组件

--- upload-component/src/App.tsx
+++ upload-component/src/App.tsx
@@ -1,36 +1,38 @@
-import React from 'react';
-import { InboxOutlined, UploadOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
-import Upload, { UploadProps } from './Upload'
+import React from "react";
+import { InboxOutlined } from "@ant-design/icons";
+// import { Button } from 'antd';
+import Upload, { UploadProps } from "./Upload";
 
 const props: UploadProps = {
-  name: 'file',
-  action: 'http://localhost:3333/upload',
+  name: "file",
+  action: "http://localhost:3333/upload",
   beforeUpload(file) {
-    if(file.name.includes('1.image')) {
+    if (file.name.includes("1.image")) {
       return false;
     }
     return true;
   },
   onSuccess(ret) {
-    console.log('onSuccess', ret);
+    console.log("onSuccess", ret);
   },
   onError(err) {
-    console.log('onError', err);
+    console.log("onError", err);
   },
   onProgress(percentage, file) {
-    console.log('onProgress', percentage);
+    console.log("onProgress", percentage);
+    console.log("file", file);
   },
   onChange(file) {
-    console.log('onChange', file);
-  }
+    console.log("onChange", file);
+  },
+  drag: false
 };
 
 const App: React.FC = () => (
   <Upload {...props} drag>
     {/* <Button icon={<UploadOutlined />}>Click to Upload</Button> */}
     <p>
-      <InboxOutlined style={{fontSize: '50px'}}/>
+      <InboxOutlined style={{ fontSize: "50px" }} />
     </p>
     <p>点击或者拖拽文件到此处</p>
   </Upload>
